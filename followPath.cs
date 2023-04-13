e2b5dd5082f2cac8c761a8ba3f46d5a2d84bc9cd
Bruno Lima <lima14032001@gmail.com>
2023-04-13 16:04:15 -0300

    rota que o personagem deve seguir no mapa

---
+++ followPath.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class followPath : MonoBehaviour
+{
+    [SerializeField] public Transform[] allwayPoints;
+    [SerializeField] public float rotationSpeed = 0.5f;
+    [SerializeField] public float movementSpeed = 0.5f;
+    [SerializeField] public int currentTarget;
+
+    private Animator maleAnim;
+
+    // Start is called before the first frame update
+    void Start() 
+    {
+        maleAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Movement();
+        Rotate();
+        ChangeTarget();
+    }
+
+    void Movement() {
+        transform.position = Vector3.MoveTowards(transform.position, allwayPoints[currentTarget].position, movementSpeed * Time.deltaTime);
+    }
+    void Rotate() {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation( allwayPoints[currentTarget].position-transform.position), rotationSpeed*Time.deltaTime);
+    }
+
+    void ChangeTarget() {
+        if (transform.position == allwayPoints[currentTarget].position) { 
+            currentTarget++; currentTarget = currentTarget % allwayPoints.Length;
+        }
+    }
+}
